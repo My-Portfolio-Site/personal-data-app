5baa083329b8f001126cff377a3a65ee27136148
Keshab Manni <keshabmanni22@gmail.com>
2025-08-09 23:23:18 +0545

    fixed in mobile screen, conversation window height.

--- components/page-formatter.tsx
+++ components/page-formatter.tsx
@@ -47,7 +47,7 @@ export function PageHeader({ title, children }: { title: string, children?: Reac
 
 export function PageContent({ children }: { children: React.ReactNode }) {
   return (
-    <div className='flex-1 flex justify-center min-h-dvh md:min-h-[calc(100dvh-var(--header-height)-16px)]'>
+    <div className='flex-1 flex justify-center min-h-[calc(100dvh-var(--header-height))] md:min-h-[calc(100dvh-var(--header-height)-16px)]'>
       <div className='mx-4 md:mx-8 my-3 md:my-5 space-y-4 max-w-[780px] w-full min-h-full'>
         {children}
       </div>
